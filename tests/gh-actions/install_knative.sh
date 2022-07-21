f714aa4a327d0bf33a42b9d974f49a88ca0c4528
Nikolaos Loukas <nickloukas@arrikto.com>
2022-07-21 13:41:40 +0300

    Fix typo in yaml.

---
+++ tests/gh-actions/install_knative.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+echo "Installing KNative ..."
+set +e
+kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
+set -e
+sleep 60
+kustomize build common/knative/knative-serving/overlays/gateways | kubectl apply -f -
+kustomize build common/knative/knative-eventing/base | kubectl apply -f -
+kustomize build common/istio-1-14/kubeflow-istio-resources/base | kubectl apply -f -
+sleep 60
