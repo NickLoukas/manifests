fdb4c6c1f22eefccc5043cf3b57b2eddd97c65b8
Nikolaos Loukas <nickloukas@arrikto.com>
2022-08-01 16:41:52 +0300

    Give permission to argo cli script.

--- tests/gh-actions/install_argo_cli.sh
+++ tests/gh-actions/install_argo_cli.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
+
 # Download the binary
-curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v3.1.3/argo-darwin-amd64.gz
+curl -sLO https://github.com/argoproj/argo-workflows/releases/download/v3.1.3/argo-linux-amd64.gz
+
 # Unzip
-gunzip argo-darwin-amd64.gz
+gunzip argo-linux-amd64.gz
+
 # Make binary executable
-chmod +x argo-darwin-amd64
+chmod +x argo-linux-amd64
+
 # Move binary to path
-sudo mv ./argo-darwin-amd64 /usr/local/bin/argo
+sudo mv ./argo-linux-amd64 /usr/local/bin/argo
